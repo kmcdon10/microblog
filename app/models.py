fda7da28a35dafe8f7d429f4e319a430ab33eebb
Kate Mcdonald <kate.mcdonald@yodle.com>
2016-03-06 19:15:30 -0500

    Adding unit tests

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from hashlib import md5
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -8,6 +9,18 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
 
     @property
     def is_authenticated(self):
@@ -28,7 +41,8 @@ class User(db.Model):
             return str(self.id)  # python 3
 
     def avatar(self, size):
-	return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
+               (md5(self.email.encode('utf-8')).hexdigest(), size)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
